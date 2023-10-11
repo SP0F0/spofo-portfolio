ef374a0c40351ff91da798268a1a85749ec1719e
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-10-11 23:07:54 +0900

    🔨 [Refactor] : 서비스 인터페이스의 패키지 이동

--- src/test/java/spofo/support/service/ServiceTestSupport.java
+++ src/test/java/spofo/support/service/ServiceTestSupport.java
@@ -1,12 +1,12 @@
 package spofo.support.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import spofo.portfolio.service.PortfolioService;
+import spofo.portfolio.controller.port.PortfolioService;
 import spofo.portfolio.service.port.PortfolioRepository;
 import spofo.stock.service.StockServerService;
-import spofo.stockhave.service.StockHaveService;
+import spofo.stockhave.controller.port.StockHaveService;
 import spofo.support.annotation.CustomServiceTest;
-import spofo.tradelog.service.TradeLogService;
+import spofo.tradelog.controller.port.TradeLogService;
 
 @CustomServiceTest
 public abstract class ServiceTestSupport {
