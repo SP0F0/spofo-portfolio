8598b5dd8380547530082478fc87803198f326d6
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-10-12 16:36:46 +0900

    🔨 [Refactor] : 필요하지 않은 주석 및 어노테이션 삭제

--- src/main/java/spofo/global/domain/exception/advice/GlobalControllerAdvice.java
+++ src/main/java/spofo/global/domain/exception/advice/GlobalControllerAdvice.java
@@ -4,10 +4,6 @@ import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.ResponseEntity.badRequest;
 import static org.springframework.http.ResponseEntity.status;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
@@ -17,24 +13,9 @@ import spofo.global.domain.exception.ErrorCode;
 import spofo.global.domain.exception.PortfolioException;
 import spofo.global.domain.exception.dto.ErrorResult;
 
-@Slf4j
 @RestControllerAdvice
-@RequiredArgsConstructor
 public class GlobalControllerAdvice {
 
-    /*
-    작성 예시입니다
-    @ExceptionHandler(PortfolioException.class)
-    public String commonExHandler(PortfolioException ex, HttpServletResponse response, Model model) {
-
-        model.addAttribute("error", ex.getMessage());
-        response.setStatus(ex.getErrorCode().getStatus().value());
-
-        return "portfolio_error";
-    }
-    */
-
-    // 종합 에러 핸들러
     @ExceptionHandler
     public ResponseEntity<ErrorResult> portfolioExHandler(PortfolioException e) {
         ErrorCode errorCode = e.getErrorCode();
@@ -45,7 +26,6 @@ public class GlobalControllerAdvice {
                 .build();
 
         return status(errorCode.getStatus()).body(errorResult);
-
     }
 
     @ExceptionHandler
@@ -65,12 +45,4 @@ public class GlobalControllerAdvice {
 
         return badRequest().body(errorResult);
     }
-
-    // 종목 추가 에러 핸들러
-    @ExceptionHandler
-    public ResponseEntity DataIntegrityViolationException(DataIntegrityViolationException e) {
-        return status(HttpStatus.BAD_REQUEST)
-                .body("찾을 수 없는 포트폴리오 아이디입니다. \n" + e.getMessage());
-    }
-
 }
