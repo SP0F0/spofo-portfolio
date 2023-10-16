cf2039ab47ac4478abd7488637d9908b5b41c1d0
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-10-16 15:28:03 +0900

    🐛 [Fix] : 잘못 수정된 클래스명 원상복구

--- src/test/java/spofo/PortfolioApplicationTests.java
+++ src/test/java/spofo/PortfolioApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class PortfolioEntityApplicationTests {
+class PortfolioApplicationTests {
 
     @Test
     void contextLoads() {
