571298812363db0b422ff4bd95081d49c2493a77
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-10-17 12:13:21 +0900

    ✨ [Feat] : HoldingStock 도메인 개발

--- src/main/java/spofo/holdingstock/domain/HoldingStock.java
+++ src/main/java/spofo/holdingstock/domain/HoldingStock.java
@@ -3,6 +3,7 @@ package spofo.holdingstock.domain;
 import java.util.List;
 import lombok.Builder;
 import lombok.Getter;
+import spofo.holdingstock.controller.request.HoldingStockRequest;
 import spofo.portfolio.domain.Portfolio;
 import spofo.tradelog.domain.TradeLog;
 
@@ -15,9 +16,9 @@ public class HoldingStock {
     private Portfolio portfolio;
     private List<TradeLog> tradeLogs;
 
-    public static HoldingStock of(HoldingStockCreate holdingStockCreate, Portfolio portfolio) {
+    public static HoldingStock of(HoldingStockRequest request, Portfolio portfolio) {
         return HoldingStock.builder()
-                .stockCode(holdingStockCreate.getStockCode())
+                .stockCode(request.getCode())
                 .portfolio(portfolio)
                 .build();
     }
