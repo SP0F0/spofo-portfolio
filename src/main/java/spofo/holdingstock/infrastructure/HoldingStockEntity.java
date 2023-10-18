33fd32bc0d94356d0e9798bf320cf01ea033ebce
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-10-18 19:35:29 +0900

    🔨[Refactor] : 순환참조를 방지하도록 부모 엔티티의 도메인 정보는 넣지 않도록 수정

--- src/main/java/spofo/holdingstock/infrastructure/HoldingStockEntity.java
+++ src/main/java/spofo/holdingstock/infrastructure/HoldingStockEntity.java
@@ -30,7 +30,7 @@ public class HoldingStockEntity extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String stockCode; // 종목 코드 (FK)
+    private String stockCode;
 
     @ManyToOne
     @JoinColumn(name = "portfolio_id")
@@ -49,6 +49,10 @@ public class HoldingStockEntity extends BaseEntity {
         return entity;
     }
 
+    /**
+     * 엔티티를 도메인으로 변환할 때, 부모의 정보를 넣지 않도록 하여 순환 참조를 방지한다.
+     * @return
+     */
     public HoldingStock toModel() {
         List<TradeLog> tradeLogs = tradeLogEntities.stream()
                 .map(TradeLogEntity::toModel)
@@ -57,7 +61,6 @@ public class HoldingStockEntity extends BaseEntity {
         return HoldingStock.builder()
                 .id(id)
                 .stockCode(stockCode)
-                //.portfolio(portfolioEntity.toModel())
                 .tradeLogs(tradeLogs)
                 .build();
     }
