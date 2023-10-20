020178476c5ffeaad738213547a0d0c2221551a4
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-10-20 10:22:48 +0900

    🔨[Refactor] : FakeAuthServerService 클래스명 변경에 따른 수정

--- src/test/java/spofo/support/controller/ControllerTestSupport.java
+++ src/test/java/spofo/support/controller/ControllerTestSupport.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import spofo.global.config.restclient.RestClientConfig;
-import spofo.mock.FakeAuthServerServiceImpl;
+import spofo.mock.FakeAuthServerService;
 import spofo.portfolio.controller.PortfolioController;
 
 /**
@@ -15,7 +15,7 @@ import spofo.portfolio.controller.PortfolioController;
  */
 @ActiveProfiles("test")
 @WebMvcTest(controllers = {PortfolioController.class})
-@Import({FakeAuthServerServiceImpl.class, RestClientConfig.class})
+@Import({FakeAuthServerService.class, RestClientConfig.class})
 public abstract class ControllerTestSupport extends ControllerTestMockBeanSupport {
 
     @Autowired
