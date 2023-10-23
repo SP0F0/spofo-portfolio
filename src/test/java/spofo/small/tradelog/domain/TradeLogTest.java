3773a403a077b57e9179510fe1746356fa6c17ae
gayeonP <pagaye@naver.com>
2023-10-23 10:49:48 +0900

    :sparkles: [Feat] : 매매이력 도메인 테스트 추가

---
+++ src/test/java/spofo/small/tradelog/domain/TradeLogTest.java
@@ -0,0 +1,34 @@
+package spofo.small.tradelog.domain;
+
+import static java.math.BigDecimal.TEN;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import spofo.holdingstock.domain.HoldingStock;
+import spofo.portfolio.domain.Portfolio;
+import spofo.tradelog.domain.TradeLog;
+import spofo.tradelog.domain.TradeLogCreate;
+
+public class TradeLogTest {
+
+    @Test
+    @DisplayName("TradeLogCreate 로 매매이력 도메인을 생성한다.")
+    void TradeLogCreateToTradeLog() {
+        // given
+        Portfolio portfolio = Portfolio.builder().build();
+        HoldingStock holdingStock = HoldingStock.builder().build();
+
+        TradeLogCreate create = TradeLogCreate.builder()
+                .price(TEN)
+                .build();
+
+        // when
+        TradeLog tradeLog = TradeLog.of(create, holdingStock);
+
+        // then
+        assertThat(tradeLog.getId()).isNull();
+        assertThat(tradeLog.getPrice()).isEqualTo(TEN);
+    }
+
+}
