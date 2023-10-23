f96902a05ca29994bc64fcbba1d6c7d26ba6bc59
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-10-23 12:31:34 +0900

    🐛 [Bug] : cors 설정을 filter를 통해 처리하므로 addCorsMappings 메서드 삭제

--- src/main/java/spofo/global/config/WebConfig.java
+++ src/main/java/spofo/global/config/WebConfig.java
@@ -4,7 +4,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import spofo.global.component.resolver.argumentresolver.LoginMemberArgumentResolver;
 
@@ -15,14 +14,5 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginMemberArgumentResolver());
-    }
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry
-                .addMapping("/**") // 프로그램에서 제공하는 URL
-                .allowedOrigins("http://spofo.net", "http://localhost:5173/")
-                .allowedMethods("*")
-                .allowCredentials(false);
     }
 }
