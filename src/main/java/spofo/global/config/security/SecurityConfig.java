8e7df7255bfb4a22a9832f7e727ce27a69d3508f
JANG-WON-JUN <jangwonjun0509@gmail.com>
2023-10-27 09:34:40 +0900

    🔨 [Refactor] : disable을 메서드 참조로 변경

--- src/main/java/spofo/global/config/security/SecurityConfig.java
+++ src/main/java/spofo/global/config/security/SecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -23,8 +24,8 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.httpBasic(config -> config.disable())
-                .csrf(config -> config.disable())
+        http.httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(config -> config.sessionCreationPolicy(STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated());
